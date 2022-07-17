fe2855e42fb3827ed5bef34d3aaf340ddba9a3da
Malo-Bqn <malo.bouquin@live.fr>
2022-07-17 11:53:34 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="icomoon/style.css">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Portfolio</title>
+    </head>
+    <body>
+        <h1>Coming soon.....</h1>
+    </body>
+</html>
